75efa481a475c4808f389742150966587af2713d
5935512009 <Pasut2541@hotmail.com>
2024-03-03 11:57:03 +0700

    update:{NavBar}

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,28 @@
+import online_shopping from '../../assets/online_shopping.png';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import './navbar.css'
+import { useState } from 'react';
+const Navbar = ()=>{
+const [manu,setManu] = useState("main");
+
+    return(
+    <div className="navbar">
+        <div className="navbar-logo">
+            <img src={online_shopping}  alt=""/>
+            <p>BAYSHOP</p>
+        </div>
+        <ul className='nav-manu'>
+            <li onClick={()=>{setManu("main")}}>Main{manu==="main"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("art_toy")}}>art toy{manu==="art_toy"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("accessories")}}>accessories{manu==="accessories"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("bag")}}>bag{manu==="bag"?<hr/>:<></>}</li>
+        </ul>
+        <div className='nav-login-cart'>
+            <button>login</button>
+            <MdOutlineShoppingCart size={30}/>
+            <div className='nav-cart-count'>0</div>
+        </div>
+    </div>)
+}
+
+export default Navbar
