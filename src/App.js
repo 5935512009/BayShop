c0ede19710d9541be8e3c9291ec020641a5d578c
5935512009 <Pasut2541@hotmail.com>
2024-03-04 11:48:45 +0700

    update:{Link react dom}

--- src/App.js
+++ src/App.js
@@ -2,19 +2,40 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import React from "react";
+import { Main } from './pages/Main';
+import { ArtToy } from './pages/ArtToy';
+import { Accessories} from './pages/Accessories'
+import {Other} from './pages/Other'
+import {Cart} from './pages/Cart'
+import {LoginSignup} from './pages/LoginSignup'
+import Hero from './components/hero/Hero';
+
 import {
   BrowserRouter, 
   Route,
   Routes
 } from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
+        <Hero/>
         <Routes>
-          <Route></Route>
+          {/* แบบที่ 1 */}
+          <Route path="/" element={<Main/>}/>
+          <Route path="/Art_toy" element={<ArtToy/>}/>
+          <Route path="/Accessories" element={<Accessories/>}/>
+          <Route path="/Other" element={<Other/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/LoginSignup' element={<LoginSignup/>}/>
+          {/* แบบที่ 2 อันนี้ยังไม่ได้ลอง  ตรงส่วนนี้เพิ่ม category มาลอง test ดู */
+          /* <Route path="/item1" element={<Other category="item11"/>}/>
+          <Route path="/item2" element={<Other category="item22"/>}/> */}
+          
+
         </Routes>
       </BrowserRouter>
       test branch Bay_3BB
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,24 +2,27 @@ import online_shopping from '../../assets/online_shopping.png';
 import { MdOutlineShoppingCart } from "react-icons/md";
 import './navbar.css'
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 const Navbar = ()=>{
 const [manu,setManu] = useState("main");
 
     return(
     <div className="navbar">
         <div className="navbar-logo">
-            <img src={online_shopping}  alt=""/>
-            <p>BAYSHOP</p>
+        <Link style={{textDecoration:'none'}} to="./"> 
+            <img src={online_shopping}  alt=""/></Link>
+            <Link style={{textDecoration:'none'}} to="./"> <p>BAYSHOP</p> </Link>
+           
         </div>
         <ul className='nav-manu'>
-            <li onClick={()=>{setManu("main")}}>Main{manu==="main"?<hr/>:<></>}</li>
-            <li onClick={()=>{setManu("art_toy")}}>art toy{manu==="art_toy"?<hr/>:<></>}</li>
-            <li onClick={()=>{setManu("accessories")}}>accessories{manu==="accessories"?<hr/>:<></>}</li>
-            <li onClick={()=>{setManu("other")}}>other{manu==="other"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("main")}}><Link style={{textDecoration:'none'}} to="./">Main</Link>{manu==="main"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("art_toy")}}><Link style={{textDecoration:'none'}} to="./art_toy">art toy</Link>{manu==="art_toy"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("accessories")}}><Link style={{textDecoration:'none'}} to="./accessories">accessories</Link>{manu==="accessories"?<hr/>:<></>}</li>
+            <li onClick={()=>{setManu("other")}}><Link style={{textDecoration:'none'}} to="./other">other</Link>{manu==="other"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>login</button>
-            <MdOutlineShoppingCart size={30}/>
+            <Link style={{textDecoration:'none'}} to='./LoginSignup'><button>login</button></Link>
+            <Link style={{textDecoration:'none'}} to='./cart'><MdOutlineShoppingCart size={30}/></Link>
             <div className='nav-cart-count'>0</div>
         </div>
     </div>)
